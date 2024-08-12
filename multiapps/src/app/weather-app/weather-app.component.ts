05bafa4d4a13f03463fecb50d05b95e8cc58c313
sripathisri <sripathisri8@gmail.com>
2024-08-12 19:26:47 +0530

    ui

--- multiapps/src/app/weather-app/weather-app.component.ts
+++ multiapps/src/app/weather-app/weather-app.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-weather-app',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ],
   templateUrl: './weather-app.component.html',
   styleUrl: './weather-app.component.css'
 })
 export class WeatherAppComponent {
+
+  city:any
+
+  sendCity(data:any){
+    const city = data;
+    console.log(city.value)
+  }
 
 }
